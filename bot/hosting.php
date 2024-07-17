2abf23ab1ea3173ed24fba23fbc798cd1644b9bd
piyushcodings <118120770+piyushcodings@users.noreply.github.com>
2024-07-17 13:51:14 +0530

    Update hosting.php

--- bot/hosting.php
+++ bot/hosting.php
@@ -1,7 +1,7 @@
 <?php
 
 $hosting1 = "ashgotpick";
-$hosting = "https://$hosting1.eu/akatsukiop";
+$hosting = "https://akatsuki-php-y2c1.onrender.com";
 
 $directorio = "/home/$hosting1/www/bot/";
 $archivos = scandir($directorio); // obtiene un arreglo con los nombres de todos los archivos en el directorio
